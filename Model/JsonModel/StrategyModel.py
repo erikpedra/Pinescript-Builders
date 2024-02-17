d7676fe1eab5790be70c962acac3fa3ce031cb7f
erikpedra <56580637+erikpedra@users.noreply.github.com>
2024-02-18 02:21:48 +0700

    Create StrategyModel.py

---
+++ Model/JsonModel/StrategyModel.py
@@ -0,0 +1,12 @@
+class StrategyModel:
+    """
+    Strategy model. Describes all the behavior and features of the strategy
+    """
+    def __init__(self):
+        self.Name = ""
+        self.Description = ""
+        self.Actions = []
+        self.Sources = []
+        self.Modules = []
+        self.Parameters = []
+        self.Debug = ""
