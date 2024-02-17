8e54bd886bbce99ace891be7983de2ff8fc79ca6
erikpedra <56580637+erikpedra@users.noreply.github.com>
2024-02-18 02:25:50 +0700

    Delete Model/Sources directory

--- Model/Sources/InstrumentSource.py
+++
@@ -1,8 +0,0 @@
-from Model.ParsedModel.IMetaSource import IMetaSource, SourceType
-
-class InstrumentSource(IMetaSource):
-    def __init__(self):
-        self.source_type = SourceType.Instrument
-        self.id = None
-        self.timeframe = None
-        self.price_type = None
